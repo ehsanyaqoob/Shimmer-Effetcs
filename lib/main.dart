59f6077a56d2458de5b0f6898898e2dcc6bde302
ehsanyaqoob <ehsanyaqoob07@gmail.com>
2022-09-16 10:59:28 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:friday_ui/shimmer_effect.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+//  this code is written in Dart language
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
